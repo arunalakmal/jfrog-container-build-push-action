949040db1efba271df4c28d91ef836c086d0160a
Aruna Lakmal <aruna.lakmal2@inter.ikea.com>
2023-01-02 12:45:06 +0100

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # JFrog Container Build Push Action
+
+This is a Composite GitHub Action to login, build and push docker/container images to the JFrog Cloud instance. 
+
+## Usage
+
+Please refer the action as below in the GitHub Action
+
+```
+- name: Build and Push Image
+  uses: arunalakmal/jfrog-container-build-push-action@v1
+  with:
+    jfrog-url: <org>.jfrog.io
+    jfrog-user: ${{ secrets.UNAME }}
+    jfrog-token: ${{ secrets.TOKEN }}
+    jfrog-repo: <docker_repo>
+    jfrog-image: <image_name>
+```
--- execute_action.sh
+++ execute_action.sh
@@ -6,6 +6,16 @@ JFROG_TOKEN=$3
 CONTAINER_REPO=$4
 CONTAINER_IMAGE=$5
 
-docker login -u${JFROG_USER} ${JFROG_ACCOUNT} -p${JFROG_TOKEN}
-docker build -t ${JFROG_ACCOUNT}/${CONTAINER_REPO}/${CONTAINER_IMAGE}:${GITHUB_SHA} .
-docker push ${JFROG_ACCOUNT}/${CONTAINER_REPO}/${CONTAINER_IMAGE}:${GITHUB_SHA}
+function docker_execute {
+    docker login -u${JFROG_USER} ${JFROG_ACCOUNT} -p${JFROG_TOKEN}
+    docker build -t ${JFROG_ACCOUNT}/${CONTAINER_REPO}/${CONTAINER_IMAGE}:${GITHUB_SHA} .
+    docker push ${JFROG_ACCOUNT}/${CONTAINER_REPO}/${CONTAINER_IMAGE}:${GITHUB_SHA}
+}
+
+if test "$#" -ne 5; then
+    echo "Invalid number of Arguments, plesase specify the correct number of arguments"
+    echo "Please refer https://github.com/arunalakmal/jfrog-container-build-push-action#readme for more information."
+    exit 1
+else
+    docker_execute
+fi
